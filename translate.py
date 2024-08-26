68a5e93e1e2fd212e377d0867394219b6abe8bc4
Posh Raj Dahal <dahal.poshraj24@gmail.com>
2024-08-26 23:05:47 +0200

    Speech to Text Translation

--- translate.py
+++ translate.py
@@ -21,5 +21,25 @@ def translate_and_print(text):
     print(f"German: {german_translation}")
 
 # Example usage
-nepali_sentence = "म खाना खानछु "
-translate_and_print(nepali_sentence)
+#nepali_sentence = "नेपालमा आज भोलि धेरै सवारी दुर्घटनाहरु भैराखेका छन् "
+from google.colab import files
+uploaded = files.upload()
+import speech_recognition as sr
+
+r = sr.Recognizer()
+audio_file = list(uploaded.keys())[0]
+with sr.AudioFile(audio_file) as source:
+    audio_text = r.listen(source)
+    try:
+        text = r.recognize_google(audio_text,language="ne")
+        print("Converting audio transcripts into text ...")
+        print(text)
+    except sr.RequestError:
+        print("Speech recognition service is currently unavailable.")
+    except sr.UnknownValueError:
+        print("Could not understand the audio.")
+
+#a=int(input("Enter the number of translations you would like to continue:"))
+#for _ in range(a):  
+user_input = text
+translate_and_print(user_input)
